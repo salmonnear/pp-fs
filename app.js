0bb195a1d1f612670544809fbc08454b7d98493d
Samuel Mennear <samuelmennear@gmail.com>
2019-08-15 19:13:25 -0400

    third

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const factoryRoutes = require('./api/routes/factories');
+
+var mongoDB = 'mongodb+srv://sam:P%40ssword1@clustertest-ejips.mongodb.net/test?retryWrites=true&w=majority';
+//var mongoDB = 'mongodb+srv://sam:' + process.env.MONGO_ATLAS_PW +'@clustertest-ejips.mongodb.net/test?retryWrites=true&w=majority';
+
+
+mongoose.connect(mongoDB, { useNewUrlParser: true});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");  // * represents allowing access to all address .could also just use address of resource
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+app.use('/factories', factoryRoutes);
+
+app.use((req,res,next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+
+
+module.exports = app;
