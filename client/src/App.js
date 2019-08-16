f62de46dcf3867454306d26b27d2e6b39ffae125
Samuel Mennear <samuelmennear@gmail.com>
2019-08-16 19:31:08 -0400

    url no port

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   componentDidMount() {
     console.log('test')
     axios
-      .get('/3005/factories')
+      .get('/https://infinite-ridge-95212.herokuapp.com/factories')
         //.get('https://infinite-ridge-95212.herokuapp.com/3005/factories')
         //.get('http://localhost:3005/factories')
         .then(res => this.setState({ factories: res.data.factories }));
@@ -23,7 +23,7 @@ class App extends Component {
   addFactory = (newFactory) => {
     axios
         
-        .post('https://infinite-ridge-95212.herokuapp.com/3005/factories', {
+        .post('https://infinite-ridge-95212.herokuapp.com/factories', {
         //.post('http://localhost:3005/factories', {
                   
           name: newFactory.name,
@@ -43,7 +43,7 @@ class App extends Component {
   // Delete Factory
   delFactory = (_id) => {
     axios
-      .delete(`https://infinite-ridge-95212.herokuapp.com/3005/factories/${_id}`)
+      .delete(`https://infinite-ridge-95212.herokuapp.com/factories/${_id}`)
       //.delete(`http://localhost:3005/factories/${_id}`)
       .then(res =>
          this.setState({ 
