5c49c10132b6aaa93c25bf71dddcd13d92197977
Samuel Mennear <samuelmennear@gmail.com>
2019-08-15 21:14:24 -0400

    changed appjs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.Port || 3004;
+const port = process.env.PORT || 3004;
 
 const server = http.createServer(app);
 
