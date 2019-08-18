c987babcf9fb87124b76e7c8ad24ed5992754967
Samuel Mennear <samuelmennear@gmail.com>
2019-08-18 03:01:47 -0400

    remove socket

--- server.js
+++ server.js
@@ -5,10 +5,4 @@ const port = process.env.PORT || 3005;
 
 const server = http.createServer(app);
 
-const io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-    console.log('hi from socket, connected');
-});
-
 server.listen(port);
